cbdc93171a01ead60fb7f232945ed7b1ba26ea1d
NeqsTech <76450252+NeqsTech@users.noreply.github.com>
2023-05-07 00:06:25 +0200

    Adapt character scene to the new inventory system. Add proper third person camera controller.

--- game/character/character.gd
+++ game/character/character.gd
@@ -3,41 +3,59 @@ class_name Character
 
 signal inventory_toggled()
 
-@export
-var inventory_data: InventoryData:
+@onready var pivot: Node3D = $Pivot
+@onready var model: MeshInstance3D = $MeshInstance3D
+
+@export var inventory_data: InventoryData:
 	get: return $InventoryComponent.inventory_data
 	set(value): $InventoryComponent.inventory_data = value
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+const SPEED = 15.0
+const JUMP_VELOCITY = 12.0
+const LERP = 7.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
+var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity") * 3
 
-func _unhandled_input(_event: InputEvent) -> void:
+func _ready() -> void:
+	var health_potion := ItemDB.get_item("nf:health_potion")
+	health_potion.quantity = 20
+	
+	inventory_data.add_item(
+		health_potion
+	)
+
+func _unhandled_key_input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("toggle_inventory"):
 		inventory_toggled.emit()
-	#if Input.is_action_just_pressed("add_item"):
-	#	inventory_data.add_item(preload("res://game/spells/fire_ball-spell.tres"), 1)
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
+	var input_dir := Input.get_vector("left", "right", "forward", "backward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	# Move towards where the camera points to
+	direction = direction.rotated(Vector3.UP, pivot.rotation.y)
+	
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
+		
+		# Interpolate from model look dir to character look dir
+		var model_rotation := Quaternion(model.transform.basis)
+		var character_rotatation := Quaternion(
+			Basis(Vector3.UP, atan2(-velocity.x, -velocity.z))
+		)
+		model.transform.basis = Basis(
+			model_rotation.slerp(character_rotatation, LERP * delta)
+		).orthonormalized()
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+	
 	move_and_slide()
--- main.gd
+++ main.gd
@@ -1,10 +1,10 @@
 extends Node
 
-@onready var character := $Character as Character
+@onready var character := ($World as World).character as Character
 @onready var game_ui := $CanvasLayer/GameUI as GameUI
 
 func _ready() -> void:
-	game_ui.init_character_inventory(character.inventory_data)
+	game_ui.initialize_character_inventory(character.inventory_data)
 
 func _on_character_inventory_toggled() -> void:
 	game_ui.toggle_character_inventory()
